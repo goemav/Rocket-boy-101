34750dfa4420566b06de1bfc0f7eda1b70ceeb4c
懒猫 <74753148+goemav@users.noreply.github.com>
2020-11-30 15:10:12 +0800

    Create 113002.实现接口.App.java

---
+++ 113002.实现接口.App.java
@@ -0,0 +1,9 @@
+package pac;
+
+public class App implements R{
+    public static void main(String[] args) {
+        System.out.println(R.girl_filePath);
+        System.out.println(R.boy_filePath);
+        System.out.println(R.array2d[0][1]);
+    }
+}

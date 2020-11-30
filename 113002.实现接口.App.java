3440d4cd7bb5a762a761a26314e0ac09a4a87ecb
懒猫 <74753148+goemav@users.noreply.github.com>
2020-11-30 15:28:29 +0800

    Update 113002.实现接口.App.java

--- 113002.实现接口.App.java
+++ 113002.实现接口.App.java
@@ -4,6 +4,10 @@ public class App implements R{
     public static void main(String[] args) {
         System.out.println(R.girl_filePath);
         System.out.println(R.boy_filePath);
-        System.out.println(R.array2d[0][1]);
+        for (int i = 0; i < R.array2d.length; i++) {
+            for (int j = 0; j < R.array2d[i].length; j++){
+                System.out.println(R.array2d[i][j]);
+            }
+        }
     }
 }
